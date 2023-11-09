ec8ff00e6252bfcd3b106ed0350761f5fa2ee95a
DiegoRaC14 <141955933+DiegoRaC14@users.noreply.github.com>
2023-11-09 01:00:09 -0600

    Add files via upload

--- Flujo de archivos - Escritura (agregar a un archivo).cpp
+++ Flujo de archivos - Escritura (agregar a un archivo).cpp
@@ -1,4 +1,4 @@
-/* Flujo de archivos: Escritura (añadir información a un n archivo) */
+/* Flujo de archivos: Escritura (añadir información a un archivo) */
 
 #include <stdio.h>
 
@@ -6,14 +6,14 @@ int main() {
     FILE *archivo;
 
     archivo = fopen("archivo2.txt", "a");  // Abre el archivo "archivo2.txt" en modo escritura
-    //Crea un nuevo archivo si no existe o añade algo si ya existe
+    //Crea un nuevo archivo si no existe o añade la cadena de texto si ya existe el archivo
 
     if (archivo == NULL) {  // Comprueba si hubo un error al abrir el archivo
         perror("Error al crear el archivo");  // Muestra un mensaje de error
         return 1;  // Sale del programa con un código de error
     }
 
-    fprintf(archivo, "Esto es lo nuevo que se agrega.\n");  // Añade una cadena de texto a lo existente en el documento
+    fprintf(archivo, "Esto es lo nuevo que se agrega!.\n");  // Añade una cadena de texto a lo existente en el documento
     fclose(archivo);  // Cierra el archivo después de usarlo
 
     return 0;  // Sale del programa con un código de éxito
